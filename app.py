ffe2a291c1b403cdba402c683641ce994da9b25f
Sandeep Pathak <125938774+spathak01@users.noreply.github.com>
2023-06-27 14:05:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+import pandas as pd
+import datetime
+
+app = Flask(__name__, template_folder='templates')
+data = pd.read_csv('data.csv')
+model = pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+        bdrm = request.form.get('bedrooms')
+        btrm = request.form.get('bathrooms')
+        sf_liv = request.form.get('sqft_living')
+        sf_lot = request.form.get('sqft_lot')
+        flr = request.form.get('floors')
+        water = request.form.get('waterfront')
+        view = int(2)
+        condition = int(3)
+        sf_ab = request.form.get('sqft_above')
+        sf_bs = request.form.get('sqft_basement')
+        year = datetime.datetime.now().year
+        city = int(35)
+        zip = int(98103)
+        country = int(0)
+        build = (request.form.get('yr_built'))
+        reno = (request.form.get('yr_renovated'))
+
+        features = np.array([year, bdrm, btrm, sf_liv, sf_lot, flr, water, view, condition, sf_ab, sf_bs, build, reno, city, zip, country]).reshape(1, -1)
+        prediction = model.predict(features)
+        price = prediction  
+
+        return render_template('index.html', result=np.round(price, 2))
+
+if __name__ == '__main__':
+    app.run(debug= True)
